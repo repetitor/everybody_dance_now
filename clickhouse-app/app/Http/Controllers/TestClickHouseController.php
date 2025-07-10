9efa579a99b07e0cae438c4b6ff063f5430d605a
Viktar Akulenka <repetitor202@gmail.com>
2025-07-11 01:42:24 +0300

    lab - test api-route

---
+++ clickhouse-app/app/Http/Controllers/TestClickHouseController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestClickHouseController extends Controller
+{
+    public function test()
+    {
+        return 123;
+    }
+}
--- clickhouse-app/routes/api.php
+++ clickhouse-app/routes/api.php
@@ -1,8 +1,12 @@
 <?php
+declare(strict_types=1);
 
+use App\Http\Controllers\TestClickHouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', [TestClickHouseController::class, 'test']);
